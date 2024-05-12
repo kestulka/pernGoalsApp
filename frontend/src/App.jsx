26939b7583c8a7b33205b78475d58740c53ea867
kestulka <kgirdiunas0@gmail.com>
2024-05-12 23:31:33 +0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <p className="bg-lime-500">Test tail</p>
+    </>
+  );
 }
 
 export default App;
