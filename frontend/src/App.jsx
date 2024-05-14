ff1d1138607acdc3e02456eaca468fd02d2f1ee3
unknown <kgirdziunas0@gmail.com>
2024-05-14 14:44:34 +0300

    before tests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import MainPage from "../src/Components/Main/MainPage";
+import LoginPage from "../src/Components/Login/LoginPage";
+import RegisterPage from "../src/Components/Register/RegisterPage";
 
 function App() {
   return (
     <>
-      <p className="bg-lime-500">Test tail</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
